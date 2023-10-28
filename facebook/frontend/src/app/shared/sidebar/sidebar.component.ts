1a9318c6e0a3e3c9db19c87aa847de36bf64df80
Boris Giovanni <bancasi@unsa.edu.pe>
2023-10-28 16:49:02 -0500

    Sidebar compoent created

--- facebook/frontend/src/app/shared/sidebar/sidebar.component.ts
+++ facebook/frontend/src/app/shared/sidebar/sidebar.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+const iconNames = [
+  { icon: 'rss_feed', name: 'Feed' },
+  { icon: 'chat', name: 'Chats' },
+  { icon: 'play_circle_filled', name: 'Videos' },
+  { icon: 'group', name: 'Groups' },
+  { icon: 'bookmark', name: 'Bookmarks' },
+  { icon: 'help_outline', name: 'Questions' },
+  { icon: 'work_outline', name: 'Jobs' },
+  { icon: 'event', name: 'Events' },
+  { icon: 'school', name: 'Courses' },
+];
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
-
+  iconNames = iconNames;
 }
