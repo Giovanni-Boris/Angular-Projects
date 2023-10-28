82faed70c58338f626809b2ec4850cb130235394
Boris Giovanni <bancasi@unsa.edu.pe>
2023-10-28 12:44:10 -0500

    Creation of template rightbar sidebar feed

--- facebook/frontend/src/app/shared/shared.module.ts
+++ facebook/frontend/src/app/shared/shared.module.ts
@@ -2,18 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopbarComponent } from './topbar/topbar.component';
 import { MatIconModule } from '@angular/material/icon';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FeedComponent } from './feed/feed.component';
+import { RightbarComponent } from './rightbar/rightbar.component';
 
 
 @NgModule({
   declarations: [
-    TopbarComponent
+    TopbarComponent,
+    SidebarComponent,
+    FeedComponent,
+    RightbarComponent
   ],
   imports: [
     CommonModule,
     MatIconModule
   ],
   exports :[
-    TopbarComponent
+    TopbarComponent,
+    RightbarComponent,
+    FeedComponent,
+    SidebarComponent
   ]
 })
 export class SharedModule { }
