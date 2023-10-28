9348085e501f9be1b7483ea0a55dbdad12ebc97b
Boris Giovanni <bancasi@unsa.edu.pe>
2023-10-28 08:23:01 -0500

    register page created

--- facebook/frontend/src/app/app-routing.module.ts
+++ facebook/frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './feature/pages';
+import { RegisterComponent } from './feature/pages/register/register.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
--- facebook/frontend/src/app/feature/feature.module.ts
+++ facebook/frontend/src/app/feature/feature.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RegisterComponent } from './pages/register/register.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule
   ]
 })
 export class FeatureModule { }
