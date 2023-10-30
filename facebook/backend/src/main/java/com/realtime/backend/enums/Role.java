525192154caed579fa9cc0ebed84ac130cf935c6
Boris Giovanni <bancasi@unsa.edu.pe>
2023-10-30 11:35:38 -0500

    User model created  with security

---
+++ facebook/backend/src/main/java/com/realtime/backend/enums/Role.java
@@ -0,0 +1,6 @@
+package com.realtime.backend.enums;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- facebook/backend/src/main/java/com/realtime/backend/model/User.java
+++ facebook/backend/src/main/java/com/realtime/backend/model/User.java
@@ -1,4 +1,77 @@
 package com.realtime.backend.model;
 
-public class User {
+import com.realtime.backend.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String usrname;
+    private String email;
+    private String password;
+    private String profilePicture;
+    private String coverPicture;
+    @OneToMany
+    private List<User> followers;
+    @OneToMany
+    private List<User> followings;
+    private String description;
+    private String country;
+    private Integer relationship;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
