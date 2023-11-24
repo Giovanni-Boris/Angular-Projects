3adb6e5571f822fb042bc425d8a3406d768b69f3
Boris Giovanni <bancasi@unsa.edu.pe>
2023-11-24 09:10:27 -0500

    sidebar compoenne created

---
+++ dashboard/front/src/app/modules/shared/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, MatIconModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+})
+export class NavbarComponent {
+  toggleDarkMode(){}
+}
